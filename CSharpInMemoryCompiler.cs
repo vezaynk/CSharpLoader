ecd350fc4a1fba50c1a8f136b292eadd3a28e5b9
Slava Knyazev <slava@knyazev.io>
2019-06-21 15:52:42 -0400

    Added documentation and moved code around

---
+++ CSharpInMemoryCompiler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System.Text;
+
+namespace CSharpLoader
+{
+    public class InMemoryCompiler
+    {
+        public Assembly Assembly { get; set; }
+        public InMemoryCompiler(string filename, string sdk)
+        {
+            // Read in code to compile
+            string codeToCompile = File.ReadAllText(filename, Encoding.UTF8);
+
+            // Generate syntax tree (Roslyn, see: https://docs.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/get-started/syntax-analysis)
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(codeToCompile);
+
+            // Generate a random assembly name to avoid collisions
+            string assemblyName = Path.GetRandomFileName();
+
+            // The C# program probably needs references (ie. System.IO)
+            // The compiled program may want to utilise the compiling program's namespaces
+            //  including all the references used by the original program makes sense
+            var references = AppDomain.CurrentDomain.GetAssemblies().Select(a => a.Location)
+                                // Sometimes those are not enough, in which case we should load the entire SDK just in case.
+                                .Union(sdk != null ? Directory.GetFileSystemEntries(sdk, "*.dll") : Enumerable.Empty<string>())
+                                // Create references to all the DLLs
+                                .Select(path => MetadataReference.CreateFromFile(path));
+            // TODO: Allow including multiple directories or specific files
+
+            // Run compilation
+            CSharpCompilation compilation = CSharpCompilation.Create(
+            assemblyName,
+            // Include syntax tree. TODO: Allow for multiple programs
+            syntaxTrees: new[] { syntaxTree },
+            // Include all references
+            references: references,
+            // Generate DLL (Static linking would result if a very large file, including the entire SDK)
+            options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+            using (var ms = new MemoryStream())
+            {
+                // Emit result into memory stream
+                EmitResult result = compilation.Emit(ms);
+                if (!result.Success)
+                {
+                    // Compilation failed. Display all errors.
+                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                    diagnostic.IsWarningAsError ||
+                    diagnostic.Severity == DiagnosticSeverity.Error);
+                    foreach (Diagnostic diagnostic in failures)
+                    {
+                        Console.Error.WriteLine("\t{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                    }
+                    throw new InvalidProgramException("Compilation failed. See errors above.");
+                }
+                else
+                {
+                    // Compilation succeeded
+                    // Rewind MemoryStream to beginning (ms.Position = size of DDL in bytes)
+                    ms.Position = 0;
+                    // Load DDL into memory
+                    Assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+                }
+            }
+        }
+        public Type GetType(string name)
+        {
+            return Assembly.GetType(name);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# C# Loader
+
+Compile C# source-code into an in-memory assembly using the Roslyn API
