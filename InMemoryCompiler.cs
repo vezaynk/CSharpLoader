b60e3b2ee89ad97e502154517687e0f764eea4b8
Slava Knyazev <slava@knyazev.io>
2019-07-03 16:20:18 -0400

    Loading all trusted references instead of trying to guess

--- InMemoryCompiler.cs
+++ InMemoryCompiler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -13,27 +14,50 @@ namespace CSharpLoader
 {
     public class InMemoryCompiler
     {
-        public Assembly Assembly { get; set; }
-        public InMemoryCompiler(IEnumerable<string> filenames, string sdk)
+        private Collection<string> Filenames { get; set; }
+        private Collection<string> CodeBodies { get; set; }
+        public InMemoryCompiler() {
+            Filenames = new Collection<string>();
+            CodeBodies = new Collection<string>();
+        }
+
+        public void AddFile(string path) {
+            Filenames.Add(path);
+        }
+        public void AddCodeBody(string codeBody) {
+            CodeBodies.Add(codeBody);
+        }
+        public Assembly CompileAssembly()
         {
-            IEnumerable<SyntaxTree> syntaxTrees = filenames
+            IEnumerable<SyntaxTree> syntaxTrees = Filenames
                                                     // Read in code to compile
                                                     .Select(filename => File.ReadAllText(filename, Encoding.UTF8))
                                                     // Generate syntax tree (Roslyn, see: https://docs.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/get-started/syntax-analysis)
+                                                    .Union(CodeBodies)
                                                     .Select(body => CSharpSyntaxTree.ParseText(body));
+
             // Generate a random assembly name to avoid collisions
             string assemblyName = Path.GetRandomFileName();
 
             // The C# program probably needs references (ie. System.IO)
             // The compiled program may want to utilise the compiling program's namespaces
             //  including all the references used by the original program makes sense
-            var references = AppDomain.CurrentDomain.GetAssemblies().Select(a => a.Location)
-                                // Sometimes those are not enough, in which case we should load the entire SDK just in case.
-                                .Union(sdk != null ? Directory.GetFileSystemEntries(sdk, "*.dll") : Enumerable.Empty<string>())
-                                // Create references to all the DLLs
-                                .Select(path => MetadataReference.CreateFromFile(path));
-            // TODO: Allow including multiple directories or specific files
+            var trustedAssembliesPaths = ((string)AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES")).Split(Path.PathSeparator);
+            
+            // var references = AppDomain.CurrentDomain.GetAssemblies()
+            //                     // Dynamic assemblies cannot be loaded this way
+            //                     .Where(a => !a.IsDynamic && !string.IsNullOrEmpty(a.Location))
+            //                         .Select(a => a.Location)
+            //                     // Sometimes those are not enough, in which case we should load the entire SDK just in case.
+            //                     .Union(SDKPath != null ? Directory.GetFileSystemEntries(SDKPath, "*.dll") : Enumerable.Empty<string>())
+            //                     .Union(AssemblyPaths)
+            //                     // Create references to all the DLLs
+            //                     .Select(path => MetadataReference.CreateFromFile(path));
 
+            var references = trustedAssembliesPaths.Select(path => MetadataReference.CreateFromFile(path));
+
+            // TODO: Allow including multiple directories or specific files
+    
             // Run compilation
             CSharpCompilation compilation = CSharpCompilation.Create(
             assemblyName,
@@ -65,13 +89,13 @@ namespace CSharpLoader
                     // Rewind MemoryStream to beginning (ms.Position = size of DDL in bytes)
                     ms.Position = 0;
                     // Load DDL into memory
-                    Assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+                    return AssemblyLoadContext.Default.LoadFromStream(ms);
                 }
             }
         }
         public Type GetType(string name)
         {
-            return Assembly.GetType(name);
+            return CompileAssembly().GetType(name);
         }
     }
 }
